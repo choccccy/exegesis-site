d3732bfcc0e9ace8b91fd61000c8b69e4a142118
choccy <choccy@oori.dev>
2025-09-30 17:34:08 -0600

    header bar mostly styled

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Exegesis';
+export const SITE_DESCRIPTION = 'Some heavens wait for nothing...';
