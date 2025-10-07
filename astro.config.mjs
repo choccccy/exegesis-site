51bbc88f2a3da5ba49b7d03b2d91168ee82a5bb2
choccy <choccy@oori.dev>
2025-10-07 16:52:54 -0600

    fix edge margins

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import remarkUnwrapImages from 'remark-unwrap-images';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://choccccy.github.io',
   // base: '/exegesis-site',
   integrations: [mdx(), sitemap()],
+  markdown: {
+    remarkPlugins: [remarkUnwrapImages]
+  },
 
   vite: {
     plugins: [tailwindcss()],
